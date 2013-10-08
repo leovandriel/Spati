c31232cf0b574db15c5897eee6b7f1ca428a649b
Leo Vandriel <git@leovandriel.com>
2013-10-08 15:05:22 +0200

    fix cache parser

--- Classes/Cache/WDSHTTPCache.h
+++ Classes/Cache/WDSHTTPCache.h
@@ -16,9 +16,9 @@
 @property (nonatomic, readonly) NSURL *baseURL;
 @property (nonatomic, readonly) NSOperationQueue *queue;
 
-- (id)initWithCaches:(NSArray *)caches parser:(WDSParser *)parser;
-- (id)initWithCaches:(NSArray *)caches parser:(WDSParser *)parser concurrent:(NSUInteger)concurrent;
-- (id)initWithCaches:(NSArray *)caches parser:(WDSParser *)parser queue:(NSOperationQueue *)queue;
+- (id)initWithCaches:(NSArray *)caches;
+- (id)initWithCaches:(NSArray *)caches concurrent:(NSUInteger)concurrent;
+- (id)initWithCaches:(NSArray *)caches queue:(NSOperationQueue *)queue;
 - (id)objectForKey:(NSString *)key force:(BOOL)force block:(void(^)(id object, BOOL cancelled))block;
 - (id)dataForKey:(NSString *)key force:(BOOL)force block:(void(^)(NSData *data, BOOL cancelled))block;
 - (void)forceFetchForKey:(NSString *)key;
