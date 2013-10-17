4c26ca9ee03f548b5aa5b459ef4a3d684d592865
Leonard van Driel <leonardvandriel@gmail.com>
2013-10-17 14:20:33 +0200

    add memory disk cache

---
+++ Classes/Cache/WDSMemoryDiskCache.m
@@ -0,0 +1,42 @@
+//
+//  WDSMemoryDiskCache.m
+//  Spati
+//
+//  Copyright (c) 2013 Wit Dot Media Berlin GmbH. All rights reserved.
+//
+
+#import "WDSMemoryDiskCache.h"
+#import "WDSMemoryCache.h"
+#import "WDSDiskCache.h"
+
+
+@implementation WDSMemoryDiskCache
+
+- (id)init
+{
+    return [self initWithName:@"WDSMemoryDiskCache"];
+}
+
+- (id)initWithName:(NSString *)name
+{
+    return [self initWithName:name parser:nil expires:0];
+}
+
+- (id)initWithName:(NSString *)name parser:(WDSParser *)parser expires:(NSTimeInterval)expires
+{
+    WDSMemoryCache *memoryCache = [[WDSMemoryCache alloc] initWithName:name parser:parser];
+    WDSDiskCache *diskCache = [[WDSDiskCache alloc] initWithName:name expires:expires];
+    return [self initWithMemoryCache:memoryCache diskCache:diskCache];
+}
+
+- (id)initWithMemoryCache:(WDSMemoryCache *)memoryCache diskCache:(WDSDiskCache *)diskCache
+{
+    self = [super initWithCaches:@[memoryCache, diskCache]];
+    if (self) {
+        _memoryCache = memoryCache;
+        _diskCache = diskCache;
+    }
+    return self;
+}
+
+@end
