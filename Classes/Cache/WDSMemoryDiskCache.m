7f0f7cad53e7d8cad2846fc817fc63abc1ba6088
Leonard van Driel <leonardvandriel@gmail.com>
2013-10-18 15:21:44 +0200

    fix memory disk cache

--- Classes/Cache/WDSMemoryDiskCache.m
+++ Classes/Cache/WDSMemoryDiskCache.m
@@ -8,6 +8,7 @@
 #import "WDSMemoryDiskCache.h"
 #import "WDSMemoryCache.h"
 #import "WDSDiskCache.h"
+#import "WDSParser.h"
 
 
 @implementation WDSMemoryDiskCache
@@ -19,24 +20,38 @@
 
 - (id)initWithName:(NSString *)name
 {
-    return [self initWithName:name expires:0];
+    return [self initWithName:name parser:nil expires:0];
 }
 
-- (id)initWithName:(NSString *)name expires:(NSTimeInterval)expires
+- (id)initWithName:(NSString *)name parser:(WDSParser *)parser expires:(NSTimeInterval)expires
 {
     WDSMemoryCache *memoryCache = [[WDSMemoryCache alloc] initWithName:name];
     WDSDiskCache *diskCache = [[WDSDiskCache alloc] initWithName:name expires:expires];
-    return [self initWithMemoryCache:memoryCache diskCache:diskCache];
+    return [self initWithMemoryCache:memoryCache diskCache:diskCache parser:parser];
 }
 
-- (id)initWithMemoryCache:(WDSMemoryCache *)memoryCache diskCache:(WDSDiskCache *)diskCache
+- (id)initWithMemoryCache:(WDSMemoryCache *)memoryCache diskCache:(WDSDiskCache *)diskCache parser:(WDSParser *)parser
 {
     self = [super initWithCaches:@[memoryCache, diskCache]];
     if (self) {
         _memoryCache = memoryCache;
         _diskCache = diskCache;
+        _parser = parser;
     }
     return self;
+}
+
+- (id)objectForKey:(NSString *)key dataOnly:(BOOL)dataOnly
+{
+    if (dataOnly) {
+        NSData *result = [_diskCache objectForKey:key dataOnly:YES];
+        if (!result && _parser) result = [_parser serialize:[_memoryCache objectForKey:key dataOnly:NO]];
+        return result;
+    } else {
+        NSData *result = [_memoryCache objectForKey:key dataOnly:NO];
+        if (!result && _parser) result = [_parser parse:[_diskCache objectForKey:key dataOnly:YES]];
+        return result;
+    }
 }
 
 @end
