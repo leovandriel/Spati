3d6513583385deffe670c1eed5d46699801c39c8
Jorn van Dijk <jorn@vdijk.com>
2015-09-16 15:38:58 -0700

    Add WDSURLSession

---
+++ Classes/Link/WDSURLSession.h
@@ -0,0 +1,13 @@
+//
+//  WDSURLSession.h
+//  Spati
+//
+//  Created by Jorn van Dijk on 16-09-15.
+//  Copyright © 2015 Wit Dot Media Berlin GmbH. All rights reserved.
+//
+
+#import "WDSHTTPLink.h"
+
+@interface WDSURLSession : NSObject<WDSHTTPSession>
+
+@end
