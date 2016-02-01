345a1babd3548e08d13d379c2a4b01d50ea9bd54
Leo Vandriel <git@leovandriel.com>
2016-02-01 13:57:45 -0800

    add queue to transform pipe

--- Classes/Pipe/WDSTransformPipe.h
+++ Classes/Pipe/WDSTransformPipe.h
@@ -17,7 +17,9 @@
 @interface WDSTransformPipe : WDSPipe
 
 @property (nonatomic, readonly) id<WDSTransform> transform;
+@property (nonatomic, strong) dispatch_queue_t queue;
 
 - (instancetype)initWithTransform:(id<WDSTransform>)transform;
+- (instancetype)initWithTransform:(id<WDSTransform>)transform queue:(dispatch_queue_t)queue;
 
 @end
--- Classes/Pipe/WDSTransformPipe.m
+++ Classes/Pipe/WDSTransformPipe.m
@@ -12,9 +12,15 @@
 
 - (instancetype)initWithTransform:(id<WDSTransform>)transform
 {
+    return [self initWithTransform:transform queue:nil];
+}
+
+- (instancetype)initWithTransform:(id<WDSTransform>)transform queue:(dispatch_queue_t)queue
+{
     self = [super init];
     if (self) {
         _transform = transform;
+        _queue = queue;
     }
     return self;
 }
@@ -23,8 +29,17 @@
 {
     return [self.next get:key block:^(id object, WDSStatus status) {
         if (status == WDSStatusSuccess) {
-            id transformed = [_transform transform:object key:key];
-            if (block) block(transformed, status);
+            if (_queue) {
+                dispatch_async(_queue, ^{
+                    id transformed = [_transform transform:object key:key];
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        if (block) block(transformed, status);
+                    });
+                });
+            } else {
+                id transformed = [_transform transform:object key:key];
+                if (block) block(transformed, status);
+            }
         } else {
             if (block) block(object, status);
         }
